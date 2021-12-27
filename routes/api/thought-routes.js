32d829ae27a05f185d2b5aa2687746d886e07800
Noah Tidwell <ntidwell34@yahoo.com>
2021-12-26 21:49:36 -0600

    thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const thoughtController = require("../../controllers/thought-controller");
+
+const {
+  getAllThoughts,
+  getThoughtsById,
+  addThoughts,
+  updateThoughts,
+  deleteThoughts,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thought-controller");
+
+//use api/thoughts
+
+router
+  .route("/")
+
+  .get(getAllThoughts)
+  .post(addThoughts);
+//use api/thoughts/:id
+router
+  .route("/:id")
+  .put(updateThoughts)
+  .get(getThoughtsById)
+  .delete(deleteThoughts);
+
+//use api/thoughts/:userId
+
+router
+  .route("/:userId")
+
+  .post(addThoughts);
+
+//use api/thoughts/:thoughtId/reactions
+router
+  .route("/:thoughtId/reactions/")
+
+  .post(addReaction);
+
+//use api/thoughts/:thoughtId/reactionId
+router
+  .route("/:thoughtId/reactions/:reactionId")
+
+  .delete(deleteReaction);
+
+module.exports = router;
