03ca62a7b1f41458562e826880b6e96b0b6933c7
Noah Tidwell <ntidwell34@yahoo.com>
2021-12-26 21:48:32 -0600

    user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/user-controller");
+
+//use api/users
+
+router
+  .route("/")
+
+  .get(getAllUsers)
+
+  .post(createUser);
+
+//use api/users/id:
+
+router
+  .route("/:id")
+
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+//use api/users/:userId/friends/:friendId
+
+router
+  .route("/:id/friends/:friendsId")
+
+  .post(addFriend)
+  .delete(deleteFriend);
+
+module.exports = router;
